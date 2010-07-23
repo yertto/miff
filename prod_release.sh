56b097b9015866e425d99749cf28320280474755
yertto <yertto@gmail.com>
2010-07-23 19:08:04 +1000

    small fix in prod_release

--- prod_release.sh
+++ prod_release.sh
@@ -3,6 +3,7 @@ editor=${EDITOR:-vi}
 version=`cat VERSION`
 
 $editor VERSION                                   &&
+version=`cat VERSION`                             &&
 git commit -am "v$version release"                &&
 git tag v`cat VERSION`                            &&
 git push github                                   &&
