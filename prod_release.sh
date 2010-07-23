7e439c7a5984208990aa619325d207ca38fe1b32
yertto <yertto@gmail.com>
2010-07-23 14:45:38 +1000

    post v0.0.3 release

---
+++ prod_release.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+editor=${EDITOR:-vi}
+version=`cat VERSION`
+
+$editor $version                                  &&
+git commit -am "v$version release"                &&
+git tag v`cat VERSION`                            &&
+git push github                                   &&
+git push --tags                                   &&
+git push production                               &&
+printf "Now, update VERSION to ($version+1).rc1 " &&
+read x                                            &&
+$editor VERSION                                   &&
+git commit -am "post v$version release"
