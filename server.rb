62984cbf50434e9c442d4243cd48d23a00f127df
yertto <yertto@gmail.com>
2010-07-23 13:53:09 +1000

    added google analytics

--- server.rb
+++ server.rb
@@ -246,6 +246,15 @@ __END__
       ,
       %a{:href=>"http://www.twitter.com/yertto"}
         %img{:src=>"http://twitter-badges.s3.amazonaws.com/t_mini-a.png", :alt=>"Follow yertto on Twitter"}
+  :javascript
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-17600890-1']);
+    _gaq.push(['_trackPageview']);
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
 
 
 @@ layout
